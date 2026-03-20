cce8ef7613c3823ef4227cbdde8d3830941d9af7
taminhdatta-ui <taminhdatta@gmail.com>
2026-03-20 22:56:09 +0700

    NangCao

---
+++ Bai19.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+//MSV: 2415053122107
+//Ho va ten: Ta Minh Dat
+// Lop: 225LTC#02
+class Bai19
+
+{
+    static void Main()
+    {
+        List<int> nums = new List<int> { 1, 3, 2, 3, 4, 3, 2, 1, 3 };
+        int mostFrequent = nums
+            .GroupBy(n => n)                          
+            .OrderByDescending(g => g.Count())        
+            .First()                                 
+            .Key;                                     
+        Console.WriteLine("So xuat hien nhieu nhat: " + mostFrequent);
+    }
+}
