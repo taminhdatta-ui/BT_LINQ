5ca0086959baa316ef3fe5848dbdb09bd9b76b61
taminhdatta-ui <taminhdatta@gmail.com>
2026-03-20 21:31:47 +0700

    CoBan

---
+++ Bai3.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+//MSV: 2415053122107
+//Ho va ten: Ta Minh Dat
+// Lop: 225LTC#02
+class Bai3
+
+{
+    static void Main()
+    {
+        List<int> numbers = new List<int> { 5, 2, 8, 1, 9, 3 };
+        var sorted = numbers.OrderBy(n => n).ToList();
+        Console.WriteLine("Tang dan: " + string.Join(", ", sorted));
+    }
+}
