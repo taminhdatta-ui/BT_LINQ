8ccb667a8dd39f2ebe9f744f5b5ee0ab8c3598f8
taminhdatta-ui <taminhdatta@gmail.com>
2026-03-20 21:42:37 +0700

    CoBan

---
+++ Bai5.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+//MSV: 2415053122107
+//Ho va ten: Ta Minh Dat
+// Lop: 225LTC#02
+class Program
+{
+    static void Main()
+    {
+        List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
+        var squares = numbers.Select(n => n * n).ToList();
+        Console.WriteLine("Bình phương: " + string.Join(", ", squares));
+    }
+}
