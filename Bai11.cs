4f4a2a9eca10651c226b802ecaefe5e8830e4e60
taminhdatta-ui <taminhdatta@gmail.com>
2026-03-20 22:23:21 +0700

    Linq_Obj

---
+++ Bai11.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+// MSV: 2415053122107
+// Ho va ten: Ta Minh Dat
+// Lop: 225LTC#02
+namespace Bai11
+{
+    internal class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Score { get; set; }
+    }
+    class Bai11
+    {
+        static void Main()
+        {
+            List<Student> students = new List<Student>()
+            {
+                new Student { Id=1, Name="An",   Score=8 },
+                new Student { Id=2, Name="Binh", Score=6 },
+                new Student { Id=3, Name="Chi",  Score=9 },
+                new Student { Id=4, Name="Dung", Score=7 }
+            };
+            var topStudent = students.OrderByDescending(s => s.Score).First();
+            Console.WriteLine($"Điem cao nhat: {topStudent.Name} - {topStudent.Score}");
+        }
+    }
+}
