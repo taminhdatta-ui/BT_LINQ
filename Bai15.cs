988038484b7002bdeaff19a9f034423a7eff5a1c
taminhdatta-ui <taminhdatta@gmail.com>
2026-03-20 22:42:57 +0700

    NangCao

---
+++ Bai15.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+//MSV: 2415053122107
+//Ho va ten: Ta Minh Dat
+// Lop: 225LTC#02
+class Bai15
+
+{
+    static void Main()
+    {
+        List<int> nums = new List<int> { 1, 2, 2, 3, 4, 4, 5 };
+        var distinct = nums.Distinct().ToList();
+        Console.WriteLine(string.Join(", ", distinct));
+    }
+}
