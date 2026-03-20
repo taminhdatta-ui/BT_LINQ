0ad61b94ca0d131982079f872640c875d8d532c0
taminhdatta-ui <taminhdatta@gmail.com>
2026-03-20 22:34:26 +0700

    Linq_Obj

---
+++ Bai14.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+// MSV: 2415053122107
+// Ho va ten: Ta Minh Dat
+// Lop: 225LTC#02
+namespace Bai14
+{
+    internal class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Score { get; set; }
+    }
+    class Bai14
+    {
+        static void Main()
+        {
+            List<Student> students = new List<Student>()
+            {
+                new Student { Id=1, Name="An",   Score=8 },
+                new Student { Id=2, Name="Binh", Score=6 },
+                new Student { Id=3, Name="Chi",  Score=9 },
+                new Student { Id=4, Name="Dung", Score=7 }
+            };
+            bool hasFailed = students.Any(s => s.Score < 5);
+            Console.WriteLine("Co sinh vien rot khong? " + hasFailed);
+        }
+    }
+}
