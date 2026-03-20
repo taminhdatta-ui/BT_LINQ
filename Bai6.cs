c24f3feb3b4fdcd083a7cceba97b30e785d6cc81
taminhdatta-ui <taminhdatta@gmail.com>
2026-03-20 21:45:20 +0700

    CoBan

---
+++ Bai6.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+//MSV: 2415053122107
+//Ho va ten: Ta Minh Dat
+// Lop: 225LTC#02
+class Bai6
+{
+    static void Main()
+    {
+        List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8 };
+        int countEven = numbers.Count(n => n % 2 == 0);
+        Console.WriteLine("So phan tu chan: " + countEven);
+    }
+}
