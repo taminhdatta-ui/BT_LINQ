5179d3e5e4e07d23896ca6b82afe9dd274dafd71
taminhdatta-ui <taminhdatta@gmail.com>
2026-03-20 21:58:34 +0700

    CoBan

---
+++ Bai7.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+//MSV: 2415053122107
+//Ho va ten: Ta Minh Dat
+// Lop: 225LTC#02
+class Bai7
+{
+    static void Main()
+    {
+        List<int> numbers = new List<int> { 1, 5, 8, 12, 3 };
+        bool hasGreaterThan10 = numbers.Any(n => n > 10);
+        Console.WriteLine("Co so lon hon  10 không? " + hasGreaterThan10);
+    }
+}
