e0d05d037b30ec51e24edeea440efdeff60fbe4c
taminhdatta-ui <taminhdatta@gmail.com>
2026-03-20 21:17:58 +0700

    CoBan

---
+++ Bai2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+//MSV: 2415053122107
+//Ho va ten: Ta Minh Dat
+// Lop: 225LTC#02
+class Bai2
+{
+    static void Main()
+    {
+        List<int> numbers = new List<int> { 1, 3, 5, 7, 9, 2, 8, 4, 6 };
+
+        var result = numbers.Where(n => n > 5).ToList();
+
+        Console.WriteLine("So lon hon 5: " + string.Join(", ", result));
+        // Output: 7, 9, 8, 6
+    }
+}
