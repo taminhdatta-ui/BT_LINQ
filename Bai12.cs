6232eb16a0f8f5bdf1450e00b773876e39511e7f
taminhdatta-ui <taminhdatta@gmail.com>
2026-03-20 22:27:53 +0700

    Linq_Obj

---
+++ Bai12.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+// MSV: 2415053122107
+// Ho va ten: Ta Minh Dat
+// Lop: 225LTC#02
+namespace Bai12
+{
+    internal class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Score { get; set; }
+    }
+    class Bai12
+    {
+        static void Main()
+        {
+            List<Student> students = new List<Student>()
+            {
+                new Student { Id=1, Name="An",   Score=8 },
+                new Student { Id=2, Name="Binh", Score=6 },
+                new Student { Id=3, Name="Chi",  Score=9 },
+                new Student { Id=4, Name="Dung", Score=7 }
+            };
+            int countPass = students.Count(s => s.Score >= 7);
+            Console.WriteLine("So sinh vien đat: " + countPass);
+        }
+    }
+}
